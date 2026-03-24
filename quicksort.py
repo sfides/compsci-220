97d70e47e50c69a4d30350d7489476694d54e98b
sfides <cmrn930@gmail.com>
2026-03-25 00:32:01 +1300

    oh my days never get sick again and miss lectures i thought i was buggin when my algorithim matched the lecture slides and didnt work turns out it was wrong and they corrected it in the lecture

--- quicksort.py
+++ quicksort.py
@@ -4,7 +4,7 @@ random.seed(67)
 def partition1(a_list):#####pivot is first element
     pivot = a_list[0]
     smaller, larger = [], []
-    for x in a_list:
+    for x in a_list[1:]:
         if x < pivot:
             smaller.append(x)
         else:
@@ -12,9 +12,10 @@ def partition1(a_list):#####pivot is first element
     return smaller, [pivot], larger
 
 def partition2(a_list):#####pivot is random element
-    pivot = a_list[random.randint(0,6)]
+    rand_num = random.randint(0,len(a_list)-1)
+    pivot = a_list[rand_num]
     smaller, larger = [], []
-    for x in a_list:
+    for x in a_list[1:]:
         if x < pivot:
             smaller.append(x)
         else:
@@ -25,11 +26,11 @@ def partition2(a_list):#####pivot is random element
 def partition3(a_list):#####pivot is median of 3 random elements from the input a_list
     find_median1 = []
     for i in range(3):
-        find_median1.append(a_list[random.randint(0,6)])
+        find_median1.append(a_list[random.randint(0,len(a_list)-1)])
     find_median1.sort()
     pivot = find_median1[1]
     smaller, larger = [], []
-    for x in a_list:
+    for x in a_list[1:]:
         if x < pivot:
             smaller.append(x)
         else:
@@ -39,11 +40,11 @@ def partition3(a_list):#####pivot is median of 3 random elements from the input 
 def partition4(a_list):#####pivot is median of 5 random elements from the input a_list
     find_median2 = []
     for i in range(5):
-        find_median2.append(a_list[random.randint(0,6)])
+        find_median2.append(a_list[random.randint(0,len(a_list)-1)])
     find_median2.sort()
     pivot = find_median2[2]
     smaller, larger = [], []
-    for x in a_list:
+    for x in a_list[1:]:
         if x < pivot:
             smaller.append(x)
         else:
@@ -53,11 +54,11 @@ def partition4(a_list):#####pivot is median of 5 random elements from the input 
 def partition5(a_list):#####pivot is median of 7 random elements from the input a_list
     find_median3 = []
     for i in range(7):
-        find_median3.append(a_list[random.randint(0,6)])
+        find_median3.append(a_list[random.randint(0,len(a_list)-1)])
     find_median3.sort()
     pivot = find_median3[3]
     smaller, larger = [], []
-    for x in a_list:
+    for x in a_list[1:]:
         if x < pivot:
             smaller.append(x)
         else:
@@ -66,7 +67,7 @@ def partition5(a_list):#####pivot is median of 7 random elements from the input 
 
 def quicksort(a_list):
     if len(a_list) <= 1: return a_list
-    smaller, equal, larger = partition(a_list)
+    smaller, equal, larger = partition5(a_list)
     smaller = quicksort(smaller)
     larger = quicksort(larger)
     return smaller + equal + larger
@@ -74,18 +75,18 @@ def quicksort(a_list):
 ###ordered elements from 0 - 5527092
 def ordered_list(ID):
     a_list = [j for j in range(0,ID)]
-    return a_list
+    return quicksort(a_list)
 ###random elements from 0 - 5527092
 def rand_list(ID):
     n = ID - 1
     a_list = []
     for k in range(0, ID):
         a_list.append(random.randint(0,n))
-    return a_list
-
+    return quicksort(a_list)
 
 ID = 5527093
-t = time.time()
+start_time = time.perf_counter()
 print(rand_list(ID))
-k = time.time()
-print(k-t)
+end_time = time.perf_counter()
+elapsed_time = end_time - start_time
+print(f"Execuion time: {elapsed_time:.4f} seconds")
